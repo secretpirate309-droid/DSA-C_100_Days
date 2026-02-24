b075ba6420c49bbbeeb738c064898ce23e4373e9
secretpirate309-droid <secretpirate309@gmail.com>
2026-02-24 12:25:31 +0530

    Add files via upload

---
+++ Day16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+
+// Given an array of integers, count the frequency of each distinct element and print the result.
+
+int main()
+{
+    int n;
+    printf("Enter Size of Array:");
+
+    int arr[n];
+
+    for(int i = 0; i<n; i++)
+    {
+        printf("Enter Element:");
+        scanf("%d", &arr[i]);
+
+    }
+
+    
+}
