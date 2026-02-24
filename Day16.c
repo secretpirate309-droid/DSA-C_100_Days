856cc0dc255451f8ae8e1fc23146945ce47f871c
secretpirate309-droid <secretpirate309@gmail.com>
2026-02-24 12:38:17 +0530

    Add files via upload

--- Day16.c
+++ Day16.c
@@ -7,15 +7,31 @@ int main()
 {
     int n;
     printf("Enter Size of Array:");
-
+    scanf("%d", &n);
     int arr[n];
 
+    
+    int freq[1001] = {0};
     for(int i = 0; i<n; i++)
     {
         printf("Enter Element:");
         scanf("%d", &arr[i]);
+        freq[arr[i]]++;
 
     }
+
+
+
+    for(int i = 0; i < n; i++) 
+    {
+        if (freq[arr[i]] > 0) {
+            printf("%d:%d ", arr[i], freq[arr[i]]);
+            freq[arr[i]] = 0; 
+        }
+    }
+    printf("\n");
+    
+    return 0;
 
     
 }
