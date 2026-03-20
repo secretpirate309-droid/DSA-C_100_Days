6204549d6351657227145ece4db9caa125566ec6
secretpirate309-droid <secretpirate309@gmail.com>
2026-03-20 09:31:29 +0530

    Add files via upload

---
+++ Day48.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Count Leaf Nodes in c
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+int countLeafNodes(struct Node* node) {
+    if (node == NULL) {
+        return 0;
+    }
+    if (node->left == NULL && node->right == NULL) {
+        return 1;
+    }
+    return countLeafNodes(node->left) + countLeafNodes(node->right);
+}
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    printf("Total Leaf Nodes: %d\n", countLeafNodes(root));
+
+    return 0;
+}
