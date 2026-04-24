c4574ff5e4f9c3975d403bf4127c1321139f3772
secretpirate309-droid <secretpirate309@gmail.com>
2026-04-24 19:56:54 +0530

    Add files via upload

---
+++ Day83.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+//Implement Bubble Sort - Implement the algorithm.
+
+
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int data[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(data) / sizeof(data[0]);
+    
+    bubbleSort(data, n);
+    printArray(data, n);
+    
+    return 0;
+}
